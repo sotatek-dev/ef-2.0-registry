e394ebc751cfb2887dbe4e60e0c6aeca52c662c3
tuanha <tuan.hoang@sotatek.com>
2022-03-10 10:56:20 +0700

    gitpkg

---
+++ lib/test/generated-wrappers/shell_sampler.d.ts
@@ -0,0 +1,59 @@
+import { EncoderOverrides, ContractTxFunctionObj, BaseContract } from '@0x/base-contract';
+import { ContractAbi, ContractArtifact, TxData, SupportedProvider } from 'ethereum-types';
+import { BigNumber } from '@0x/utils';
+import { SimpleContractArtifact } from '@0x/types';
+import { Web3Wrapper } from '@0x/web3-wrapper';
+export declare class ShellSamplerContract extends BaseContract {
+    /**
+     * @ignore
+     */
+    static deployedBytecode: string | undefined;
+    static contractName: string;
+    private readonly _methodABIIndex;
+    static deployFrom0xArtifactAsync(artifact: ContractArtifact | SimpleContractArtifact, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: (ContractArtifact | SimpleContractArtifact);
+    }): Promise<ShellSamplerContract>;
+    static deployWithLibrariesFrom0xArtifactAsync(artifact: ContractArtifact, libraryArtifacts: {
+        [libraryName: string]: ContractArtifact;
+    }, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: (ContractArtifact | SimpleContractArtifact);
+    }): Promise<ShellSamplerContract>;
+    static deployAsync(bytecode: string, abi: ContractAbi, supportedProvider: SupportedProvider, txDefaults: Partial<TxData>, logDecodeDependencies: {
+        [contractName: string]: ContractAbi;
+    }): Promise<ShellSamplerContract>;
+    /**
+     * @returns      The contract ABI
+     */
+    static ABI(): ContractAbi;
+    protected static _deployLibrariesAsync(artifact: ContractArtifact, libraryArtifacts: {
+        [libraryName: string]: ContractArtifact;
+    }, web3Wrapper: Web3Wrapper, txDefaults: Partial<TxData>, libraryAddresses?: {
+        [libraryName: string]: string;
+    }): Promise<{
+        [libraryName: string]: string;
+    }>;
+    getFunctionSignature(methodName: string): string;
+    getABIDecodedTransactionData<T>(methodName: string, callData: string): T;
+    getABIDecodedReturnData<T>(methodName: string, callData: string): T;
+    getSelector(methodName: string): string;
+    /**
+     * Sample buy quotes from Shell pool contract
+      * @param pool Address of the Shell pool contract
+      * @param takerToken Address of the taker token (what to sell).
+      * @param makerToken Address of the maker token (what to buy).
+      * @param makerTokenAmounts Maker token buy amount for each sample.
+     */
+    sampleBuysFromShell(pool: string, takerToken: string, makerToken: string, makerTokenAmounts: BigNumber[]): ContractTxFunctionObj<BigNumber[]>;
+    /**
+     * Sample sell quotes from the Shell pool contract
+      * @param pool Address of the Shell pool contract
+      * @param takerToken Address of the taker token (what to sell).
+      * @param makerToken Address of the maker token (what to buy).
+      * @param takerTokenAmounts Taker token sell amount for each sample.
+     */
+    sampleSellsFromShell(pool: string, takerToken: string, makerToken: string, takerTokenAmounts: BigNumber[]): ContractTxFunctionObj<BigNumber[]>;
+    constructor(address: string, supportedProvider: SupportedProvider, txDefaults?: Partial<TxData>, logDecodeDependencies?: {
+        [contractName: string]: ContractAbi;
+    }, deployedBytecode?: string | undefined, encoderOverrides?: Partial<EncoderOverrides>);
+}
+//# sourceMappingURL=shell_sampler.d.ts.map
